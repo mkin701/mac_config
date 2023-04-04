ff3e53274d4bb680d59ba58d2129b2969d976cec
mkin701 <cloud@mk701.com>
2023-04-04 13:43:55 -0400

    v1 Mac Config

---
+++ setup_mac.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+packages=(
+    "git"
+    "awscli"
+    "aws-vault"
+    "docker"
+    "kubernetes-cli"
+    "jq"
+    "gh"
+    "helm"
+    "kustomize"
+    "exa"
+    "kind"
+    "python"
+    "pyenv"
+    "pyenv-virtualenv"
+    "postgresql"
+    "coreutils"
+    "pass"
+    "watch"
+    "terraform"
+    "tfenv"
+    "tgenv"
+    "saml2aws"
+    "node"
+    "neofetch"
+    "nvm"
+    "go"
+    "go-task"
+    "gopls"
+    "iterm2"
+)
+
+casks=(
+    "google-chrome"
+    "github"
+    "rectangle"
+    "slack"
+    "firefox"
+    "visual-studio-code"
+    "google-drive"
+    "postman"
+)
+
+# Install XCode Select
+
+echo "Installing XCode Select"
+xcode-select --install
+
+
+# Install homebrew, if we don't have it already
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update homebrew recipies
+echo "Updating homebrew..."
+brew update
+
+# Install homebrew packages
+echo "Installing homebrew packages..."
+for package in "${packages[@]}"
+do
+  echo "Installing $package..."
+  brew install --cask $package
+done
+
+# Configure neofetch
+echo "[[ ! -f $(which neofetch) ]] || neofetch" >> ~/.zshrc
+
+
+brew cleanup
+
+# Install macOS applications
+echo "Installing macOS applications..."
+for cask in "${casks[@]}"
+do
+  echo "Installing $cask..."
+  brew install --cask $cask
+done
